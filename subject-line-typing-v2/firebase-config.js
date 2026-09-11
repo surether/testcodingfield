5e7d2c8005e6ad20331cf7cfb258dff48e567357
surether <rheeho2000@gmail.com>
2026-09-11 11:26:36 +0900

    Add v2 Firebase config template

---
+++ subject-line-typing-v2/firebase-config.js
@@ -0,0 +1,4 @@
+// Firebase를 사용하지 않을 때는 이 파일을 그대로 두십시오.
+window.SUBJECT_LINE_FIREBASE_CONFIG = null;
+// 예: window.SUBJECT_LINE_FIREBASE_CONFIG = { apiKey:'...', authDomain:'...', projectId:'...', appId:'...' };
+window.SUBJECT_LINE_TEACHER_EMAILS = [];
