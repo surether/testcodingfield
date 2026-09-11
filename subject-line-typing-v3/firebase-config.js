62f64d5fab24367be834ea2ad3874e123dd3420f
surether <rheeho2000@gmail.com>
2026-09-11 12:14:31 +0900

    Add v3 Firebase configuration template

---
+++ subject-line-typing-v3/firebase-config.js
@@ -0,0 +1,15 @@
+// Firebase 연결 전에는 아래 값을 null로 유지하십시오.
+window.SUBJECT_LINE_FIREBASE_CONFIG = null;
+
+// Firebase Console > 프로젝트 설정 > 내 앱 > 웹 앱 설정값 예시:
+// window.SUBJECT_LINE_FIREBASE_CONFIG = {
+//   apiKey: "...",
+//   authDomain: "...firebaseapp.com",
+//   projectId: "...",
+//   storageBucket: "...firebasestorage.app",
+//   messagingSenderId: "...",
+//   appId: "..."
+// };
+
+// 허용할 교사 이메일. firestore.rules와 동일하게 맞추십시오.
+window.SUBJECT_LINE_TEACHER_EMAILS = [];
